c18b945a5f282ac664927b06a79998c43f877109
MIsho <shimonchick@gmail.com>
2018-04-02 11:47:45 +0200

    Added sound

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour {
     Rigidbody rigidbody;
+    AudioSource audioSource;
 	// Use this for initialization
 	void Start () {
         rigidbody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
@@ -19,6 +21,13 @@ public class Rocket : MonoBehaviour {
     {
         if (Input.GetKey(KeyCode.Space)) {
             rigidbody.AddRelativeForce(Vector3.up);
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+            else {
+                audioSource.Stop();
+            }
         }
         if (Input.GetKey(KeyCode.W)) {
             print("Up key");
