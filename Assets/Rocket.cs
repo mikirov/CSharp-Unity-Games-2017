44d777179f7b72d93a16be994b77e65eeb2a9152
MIsho <shimonchick@gmail.com>
2018-04-02 11:24:37 +0200

    Changed rocket ship

---
+++ Assets/Rocket.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+    Rigidbody rigidbody;
+	// Use this for initialization
+	void Start () {
+        rigidbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ProcessInput();
+	}
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space)) {
+            rigidbody.AddRelativeForce(Vector3.up);
+        }
+        if (Input.GetKey(KeyCode.W)) {
+            print("Up key");
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.forward);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            print("down key");
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.back);
+        }
+    }
+}
