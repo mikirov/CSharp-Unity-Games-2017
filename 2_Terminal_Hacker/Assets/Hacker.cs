4f38ba42ba6f87eceff2964ca2934390d7cc4fcf
MIsho <shimonchick@gmail.com>
2018-04-02 18:04:10 +0200

    Reorganising project

---
+++ 2_Terminal_Hacker/Assets/Hacker.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hacker : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        this.ShowMainMenu();
+    }
+    void ShowMainMenu() {
+        Terminal.WriteLine("Choose difficulty");
+        Terminal.WriteLine("1. Easy.");
+        Terminal.WriteLine("2. Medium.");
+        Terminal.WriteLine("3. Hard.");
+        Terminal.WriteLine("Enter your selection:");
+    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
